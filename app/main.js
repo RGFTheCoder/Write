66aff764bf28510d124b1c36787b26fd7b1e0282
BuildTools <totogonzalezfernandez@gmail.com>
2018-05-06 12:30:28 -0400

    upd

--- app/main.js
+++ app/main.js
@@ -1,5 +1,9 @@
+const { dialog } = require('electron').remote;
+var fs = require('fs');
 var currFoc = null;
 var currPos = 0;
+
+
 addEventListener('load', function() {
 
     var ipc = require('electron').ipcRenderer;
@@ -26,7 +30,7 @@ addEventListener('load', function() {
 
     var bodin = document.querySelectorAll("div.body *");
     bodin.forEach(bodins => {
-        bodins.setAttribute("data-tag", bodins.tagName)
+        bodins.setAttribute("data-tag", bodins.tagName.toLowerCase());
         bodins.onclick = function() {
             currFoc = bodins;
             currPos = bodins.textContent.length;
@@ -34,6 +38,7 @@ addEventListener('load', function() {
     });
 
     var newt = false;
+    var cmd = false;
 
     var newnode = "";
 
@@ -80,6 +85,21 @@ addEventListener('load', function() {
             return;
         }
 
+        if (cmd) {
+            switch (e.key) {
+                case "s":
+                    saveFile(document.querySelector("div.body").innerHTML);
+                    break;
+                case "o":
+                    openFile();
+                    var bodin = document.querySelectorAll("div.body *");
+
+                    break;
+            }
+            cmd = false;
+            return;
+        }
+
         if (k == 8) {
             if (currPos != 0) {
                 currFoc.textContent = currFoc.textContent.split("").slice(0, currPos - 1).join("") + currFoc.textContent.split("").slice(currPos).join("");
@@ -93,8 +113,10 @@ addEventListener('load', function() {
             currPos++
         } else if (k == 37) {
             currPos--;
-        } else if (k == 16 || k == 17 || k == 38 || k == 40) {
+        } else if (k == 16 || k == 38 || k == 40) {
 
+        } else if (k == 17) {
+            cmd = true;
         } else if (k == 13) {
             newt = true;
             newnode = "";
@@ -117,3 +139,57 @@ addEventListener('load', function() {
         }
     }
 })
+var savLoc = "";
+
+function saveFile(content) {
+    if (savLoc == "") {
+        // You can obviously give a direct path without use the dialog (C:/Program Files/path/myfileexample.txt)
+        dialog.showSaveDialog((fileName) => {
+            if (fileName === undefined) {
+                console.log("You didn't save the file");
+                return;
+            }
+
+            savLoc = fileName;
+
+            // fileName is a string that contains the path and filename created in the save file dialog.  
+            fs.writeFile(fileName, content, (err) => {
+                if (err) {
+                    alert("An error ocurred creating the file " + err.message)
+                }
+
+            });
+        });
+    } else {
+        fs.writeFile(savLoc, content, (err) => {
+            if (err) {
+                alert("An error ocurred creating the file " + err.message)
+            }
+        });
+    }
+}
+
+function openFile() {
+    var t = "";
+    dialog.showOpenDialog((fileNames) => {
+        // fileNames is an array that contains all the selected
+        if (fileNames === undefined) {
+            console.log("No file selected");
+            return;
+        }
+
+        savLoc = fileNames[0];
+
+        var s = fs.readFileSync(fileNames[0], 'utf-8');
+        document.querySelector("div.body").innerHTML = s;
+
+        var bodin = document.querySelectorAll("div.body *");
+        bodin.forEach(bodins => {
+            bodins.setAttribute("data-tag", bodins.tagName)
+            bodins.onclick = function() {
+                currFoc = bodins;
+                currPos = bodins.textContent.length;
+            }
+        });
+    });
+}
--- main.js
+++ main.js
@@ -8,9 +8,9 @@ var mainWindow = null;
 app.on('ready', function() {
     mainWindow = new BrowserWindow({
         height: 600,
-        width: 800
-            // frame: false,
-            // transparent: true
+        width: 800,
+        frame: false,
+        transparent: true
     });
 
     mainWindow.loadURL('file://' + __dirname + '/app/index.html');
